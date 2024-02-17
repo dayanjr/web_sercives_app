264e31590c91185afc78de1099cb41377d867b70
dayanjr <dayanfrazao@byui.edu>
2024-02-17 14:39:46 -0700

    up

--- routes/user.js
+++ routes/user.js
@@ -8,5 +8,6 @@ router.get('/', userController.getAll);
 router.get('/:username', userController.getUser);
 
 router.post('/', userController.create);
-
+router.put('/:username', userController.updateUser);
+router.delete('/:username', userController.deleteUser);
 module.exports = router;
