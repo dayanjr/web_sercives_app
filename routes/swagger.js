a598ed38377d046f05e5068a1d4a291873963026
dayanjr <dayanfrazao@byui.edu>
2024-02-24 15:23:32 -0700

    up

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
+const { auth, requiresAuth } = require('express-openid-connect');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swaggerDesign.json'); // don't want to override this with npm run swagger
 router.use('/api-docs', swaggerUi.serve);
-router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+router.get('/api-docs', requiresAuth(),swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+require('dotenv').config();
 const port = process.env.PORT || 8080;
 const app = express();
+const { auth, requiresAuth } = require('express-openid-connect');
 
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASEURL,
+  clientID: process.env.CLIENTID,
+  issuerBaseURL: process.env.ISSUEBASERURL
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+})
 app
   .use(bodyParser.json())
   .use((req, res, next) => {
